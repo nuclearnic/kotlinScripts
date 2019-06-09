f35b443a148b581d162946b6d1b907818f3d4db8
nuclearnic <nuclearnicdev@gmail.com>
2019-06-09 14:07:13 +0200

    Initial commit

---
+++ nullables.kt
@@ -0,0 +1,19 @@
+fun parseInt(str: String): Int? {
+  return try {
+    str.toInt()
+  } catch(e: NumberFormatException) {
+    println("Error: $e")
+    return null
+  }
+}
+
+fun main(args: Array<String>) {
+  var x = parseInt("12345")
+  // var x = parseInt("abcdef")
+
+  if (x == null) {
+    println("$x is not a number")
+  } else {
+    println("$x is a number")
+  }
+}
